414bf1139148046210edcc20b027c3f9af4114c0
kisb <kisidab@stud.uni-obuda.hu>
2025-01-19 15:02:03 +0100

    v117 release

--- frontend/src/config_handler.js
+++ frontend/src/config_handler.js
@@ -6,9 +6,13 @@ export async function getAircraftConfig(instruments) {
 
     let aircraft = await getAircraftFile();
     aircraft = aircraft.toLowerCase()
+    console.log(addons.aircraft_addons)
     for (let i = 0; i < addons.aircraft_addons.length; i++) {
 
         if (aircraft.includes(addons.aircraft_addons[i].title.toLowerCase())) {
+            addons.aircraft_addons[i].version = addons.version;
+            addons.aircraft_addons[i].updated = addons.updated;
+            addons.aircraft_addons[i].app_version = addons.app_version;
             return addons.aircraft_addons[i];
         }
         // }
@@ -18,6 +22,9 @@ export async function getAircraftConfig(instruments) {
         for (let k = 0; k < addons.aircraft_addons[i].custom_popout.length; k++) {
             for (let j = 0; j < instruments.length; j++) {
                 if (instruments[j].instrument.includes(addons.aircraft_addons[i].custom_popout[k])) {
+                    addons.aircraft_addons[i].version = addons.version;
+                    addons.aircraft_addons[i].updated = addons.updated;
+                    addons.aircraft_addons[i].app_version = addons.app_version;
                     return addons.aircraft_addons[i];
                 }
             }
